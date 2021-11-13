0c9570e282f5f2ce5e5a17dbe8742c19aeacd0f0
IdarlanGomes <idarlan.gomes@gmail.com>
2021-11-12 22:07:14 -0300

    lamp quebrada atualizada

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const liga = document.getElementById ('liga')
 const desliga = document.getElementById ('desliga')
 const lamp = document.getElementById ('lamp')
 
+function isLampBroken (){
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
 function lampOn(){
+    if (!isLampBroken ()){
     lamp.src = 'img/ligada.jpg'
+    }
 }
 
 function lampOff(){
+    if (!isLampBroken ()){
     lamp.src = 'img/desligada.jpg'
+    }
 }
 
 function lampBroken(){
