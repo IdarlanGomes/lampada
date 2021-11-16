3c9e5fa722a7b58108622d4c4b3071bdf1567548
IdarlanGomes <idarlan.gomes@gmail.com>
2021-11-16 20:45:51 -0300

    botão único liga Desliga

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const liga = document.getElementById ('liga')
-const desliga = document.getElementById ('desliga')
+//const liga = document.getElementById ('liga')
+//const desliga = document.getElementById ('desliga')
+const ligaDesliga = document.getElementById ('ligaDesliga')
 const lamp = document.getElementById ('lamp')
 
 function isLampBroken (){
@@ -22,9 +23,22 @@ function lampBroken(){
     lamp.src = 'img/quebrada.jpg'
 }
 
+function lampOnOff(){
+    if (ligaDesliga.textContent == 'Liga'){
+        lampOn ()
+        ligaDesliga.textContent = 'Desliga'
 
-liga.addEventListener ('click', lampOn)
-desliga.addEventListener ('click', lampOff)
+    }else{
+        lampOff ()
+        ligaDesliga.textContent = 'Liga'
+
+    }
+}
+
+
+//liga.addEventListener ('click', lampOn)
+//desliga.addEventListener ('click', lampOff)
+ligaDesliga.addEventListener ('click', lampOnOff)
 lamp.addEventListener ('dblclick', lampBroken)
 lamp.addEventListener ('mouseover' , lampOn )
 lamp.addEventListener ('mouseout', lampOff)
